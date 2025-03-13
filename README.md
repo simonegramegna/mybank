0875b3819ee866cd1de1c22e97a299402d944dea
simo_lenovo <gramegna.simone@gmail.com>
2025-03-13 12:11:47 +0100

    uploaded base files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mybank
+ 
+Example project of bank managment system written in Java/Spring boot
---
+++ src/test/java/it/demo/mybank/MybankApplicationTests.java
@@ -0,0 +1,13 @@
+package it.demo.mybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
